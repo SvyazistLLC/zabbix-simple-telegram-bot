31e0b6a39d8a1f977f6003e071f0eb0a661119dd
sergey12313 <sergey12313@gmail.com>
2020-10-21 10:12:50 +0300

    write bot

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import args from 'args';
+import axios from 'axios';
 
 args
+  .option('token', 'bot token')
   .option('message', 'Your message')
   .option('subject', 'Your subject')
   .option('to', 'send to');
 
-const flags = args.parse(process.argv);
+const {to: chat_id, message, subject, token} = args.parse(process.argv);
 
-console.log(flags);
+const text = `${subject}
+${message}
+`;
+
+const url = `https://api.telegram.org/bot${token}/sendMessage`;
+
+axios.post(url, {
+  chat_id,
+  text,
+});
