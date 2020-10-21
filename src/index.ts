baa87bbd91a89d13161baf5cdae7bc9d76b55a64
sergey12313 <sergey12313@gmail.com>
2020-10-21 11:35:07 +0300

    add loger

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 import args from 'args';
 import axios from 'axios';
+import winston from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+require('winston-daily-rotate-file');
+
+const transport = new winston.transports.DailyRotateFile({
+  filename: 'application-%DATE%.log',
+  datePattern: 'YYYY-MM-DD-HH',
+  zippedArchive: true,
+  maxSize: '20m',
+  maxFiles: '14d',
+});
+
+const loger = winston.createLogger({transports: [transport]});
 
 args
   .option('token', 'bot token')
@@ -8,6 +21,8 @@ args
   .option('to', 'send to');
 
 const {to: chat_id, message, subject, token} = args.parse(process.argv);
+
+loger.info(JSON.stringify({chat_id, message, subject, token}));
 
 const text = `${subject}
 ${message}
